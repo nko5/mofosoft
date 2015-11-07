ebcf811dffe77b8c7fac3755a903e2292cac0ccb
Andrei Railean <andrei@siter.com.au>
2015-11-07 12:39:04 +1100

    remove unneded files

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var express = require('express');
 var path = require('path');
 var cors = require('cors');
 var app = express();
-var jwt = require('express-jwt');
-var dotenv = require('dotenv');
 
 var methodOverride = require('method-override');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var errorHandler = require('errorhandler');
+var jwt = require('express-jwt');
+var dotenv = require('dotenv');
 
 app.set('port', process.env.PORT || process.env.npm_package_config_port || 3000);
 app.use(bodyParser.json());
@@ -18,15 +18,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.configure(function() {
-//
-//
-//   app.use(app.router);
-// });
-//
-// //Get the dummy data
-// require('./server/ddata.js');
 
 app.get('/', function(req, res) {
   res.send(200, {text: "All Good In the Hood!"});
