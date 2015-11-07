59bc8b6b5c1a8b6c3b6d9ddaee7d47bd913fc4cb
Andrei Railean <andrei@siter.com.au>
2015-11-07 12:08:20 +1100

    upgrading to express 4.0 and adding the requirements for auth

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
+var http = require('http');
 var express = require('express');
+var path = require('path');
+var cors = require('cors');
 var app = express();
+var jwt = require('express-jwt');
+var dotenv = require('dotenv');
 
-//Create a static file server
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
+var methodOverride = require('method-override');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var errorHandler = require('errorhandler');
+
+app.set('port', process.env.PORT || process.env.npm_package_config_port || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.configure(function() {
+//
+//
+//   app.use(app.router);
+// });
+//
+// //Get the dummy data
+// require('./server/ddata.js');
+
+app.get('/', function(req, res) {
+  res.send(200, {text: "All Good In the Hood!"});
 });
 
-//Get the dummy data
-require('./server/ddata.js');
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
 
-var port = 8080;
-app.listen(port);
-console.log('Express server started on port %s', port);
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
